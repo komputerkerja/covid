447539c42763194ecb75003ee8bf1d3ea64a3bf3
komputerkerja <komputerkerja100@gmail.com>
2021-10-23 20:24:14 +0700

    update to heroku

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const PORT = 5000 || process.env.PORT
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, (req,res) => {
+    res.sendFile(__dirname + "/dist/index.html")
+})
+
+app.listen(PORT, () => console.log('server runing on port ' + PORT))
