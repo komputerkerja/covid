07a954555a22b268412d40d59d6a8359e722aee8
komputerkerja <komputerkerja100@gmail.com>
2021-10-23 20:43:19 +0700

    update covidx

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = 5000 || process.env.PORT
+const PORT = 8080 || process.env.PORT
 
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, (req,res) => {
